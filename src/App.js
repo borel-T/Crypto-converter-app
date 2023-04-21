b856412f8f420651866ca97b737047fe26bb6efd
borel-T <borel-T@users.noreply.github.com>
2023-04-21 13:58:20 +0300

    initial app structure and sass setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./sass/app.scss";
+import Navbar from "./components/navbar";
+import ConverterForm from "./components/converterForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <ConverterForm />
     </div>
   );
 }
