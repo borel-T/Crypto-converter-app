be5070fbc1d5beff7c69051fd7cba3147619fa24
borel-T <borel-T@users.noreply.github.com>
2023-04-22 18:27:54 +0300

    added navbar component

---
+++ src/components/navbar/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "../button";
+
+function Navbar() {
+  // handle connect
+  const _handleConnect = () => {
+    alert("Connect wallet....");
+  };
+
+  return (
+    <nav class="navbar  sticky-top justify-content-between app-navbar">
+      <div className="container">
+        <div class="app-logo" />
+        <Button
+          text="Connect wallet"
+          styling="fancy"
+          onClick={_handleConnect}
+        />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
