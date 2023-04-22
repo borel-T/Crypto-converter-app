8ba0cdc8cb64d921ff5c5a712951c525c84368eb
borel-T <borel-T@users.noreply.github.com>
2023-04-22 18:27:29 +0300

    added button component

---
+++ src/components/button/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+// button types => button | submit
+// button styling => regular | fancy
+
+function Button({ text = "", type = "button", styling = "", onClick = null }) {
+  if (styling === "fancy") {
+    return (
+      <div className="app-button-fancy">
+        <button className="app-button" onClick={onClick} type={type}>
+          {text}
+        </button>
+      </div>
+    );
+  }
+  // default button shape
+  return (
+    <button className="app-button" onClick={onClick}>
+      {text}
+    </button>
+  );
+}
+
+export default Button;
