3045645c66bbde94d07ef8f44733e6bc3b00f26e
borel-T <borel-T@users.noreply.github.com>
2023-04-24 20:19:46 +0300

    added crypto analytic page

---
+++ src/pages/cryptoAnalytics.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CryptoAnalytics() {
+  return (
+    <div className="container">
+      <div className="crypto-data-page p-4">Crypto data page</div>
+    </div>
+  );
+}
+
+export default CryptoAnalytics;
